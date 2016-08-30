1520a96093a9b16adacc976dd0846bc1335aef93
NoelBao <101703012@nccu.edu.tw>
2016-08-31 01:42:35 +0800

    Update build_ffmpeg.sh

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -9,14 +9,23 @@ mkdir -p $HOME/ffmpeg_bin && \
 \
 apt update && \
 apt install wget curl tar git mercurial -y && \
-apt install autoconf automake build-essential libass-dev libfreetype6-dev libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev pkg-config texinfo zlib1g-dev cmake-curses-gui -y && \
-apt install yasm libx264-dev libfdk-aac-dev libmp3lame-dev libopus-dev libxv-dev libxvmc-dev libxvidcore-dev -y && \
+apt install autoconf automake build-essential \
+            libass-dev libfreetype6-dev libsdl1.2-dev \
+            libtheora-dev libtool libva-dev libvdpau-dev \
+            libvorbis-dev libxcb1-dev libxcb-shm0-dev \
+            libxcb-xfixes0-dev pkg-config texinfo \
+            zlib1g-dev cmake-curses-gui -y && \
+apt install yasm libx264-dev libfdk-aac-dev \
+            libmp3lame-dev libopus-dev libxv-dev \
+            libxvmc-dev libxvidcore-dev -y && \
 \
 \
 cd $HOME/ffmpeg_sources && \
 hg clone https://bitbucket.org/multicoreware/x265 && \
 cd $HOME/ffmpeg_sources/x265/build/linux && \
-cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source && \
+cmake -G "Unix Makefiles" \
+      -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" \
+      -DENABLE_SHARED:bool=off ../../source && \
 make -j$jval && \
 make install && \
 \
@@ -25,7 +34,10 @@ cd $HOME/ffmpeg_sources && \
 wget http://storage.googleapis.com/downloads.webmproject.org/releases/webm/libvpx-1.5.0.tar.bz2 && \
 tar xjf libvpx-1.5.0.tar.bz2 && \
 cd $HOME/ffmpeg_sources/libvpx-1.5.0 && \
-./configure --prefix="$HOME/ffmpeg_build" --disable-examples --disable-unit-tests --disable-shared && \
+./configure --prefix="$HOME/ffmpeg_build" \
+            --disable-examples \
+            --disable-unit-tests \
+            --disable-shared && \
 make -j$jval && \
 make install && \
 \
@@ -61,5 +73,7 @@ make install && \
 \
 \
 mv $HOME/ffmpeg_bin/* $HOME && \
-rm -rf $HOME/ffmpeg_*
-
+rm -rf $HOME/ffmpeg_* && \
+\
+\
+echo "done!"
